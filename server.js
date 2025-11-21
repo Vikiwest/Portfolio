0efd3f579698eca1dbd359fa086b19f740124599
Vikiwest <chidiolorunda@gmail.com>
2025-11-21 17:45:48 +0100

    New reformed portfolio API

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const emailLimiter = rateLimit({
 const allowedOrigins = [
   "https://portfoliofront-nppt.onrender.com",
   "http://localhost:3000",
+  "http://localhost:3001",
   "http://localhost:5173",
 ];
 
